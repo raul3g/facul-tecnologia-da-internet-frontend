947ac742138dc02948279d0921bb8903f45cc8be
raul3g <raulsilva.developer@gmail.com>
2019-11-08 19:04:06 -0300

    Added saga auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
 
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Router } from "react-router-dom";
+
+import Routes from "./routes";
+import history from "./services/history";
+import { store, persistor } from "./store";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+        </Router>
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default App;
